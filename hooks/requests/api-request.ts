b550dd71ffa2c1668c8646fe289b1450e6d8818e
Phdrox <phximenes2103@gmail.com>
2026-03-17 09:44:57 -0300

    allow-origin

--- hooks/requests/api-request.ts
+++ hooks/requests/api-request.ts
@@ -18,19 +18,19 @@ type IdAxios={
 
 export const getApi= async ({id,url}:getAxios)=>{
   if (!url) return
-       const data=await axios.get(`https://cloud-sheet.vercel.app/${url}`,{withCredentials:true}).then(res=>res.data)
+       const data=await axios.get(`https://cloud-sheet.vercel.app/${url}`,{withCredentials:true,headers:{'Content-Type':'application/json','Access-Control-Allow-Origin':'*'}}).then(res=>res.data)
        return data
 
 }
 
 export const postApi=({url,data}:postAxios)=>{
-   return axios.post(`https://cloud-sheet.vercel.app/${url}`,data,{withCredentials:true})
+   return axios.post(`https://cloud-sheet.vercel.app/${url}`,data,{withCredentials:true,headers:{'Content-Type':'application/json','Access-Control-Allow-Origin':'*'}})
 }
 
 export const putApi=({url,data,id}:IdAxios)=>{
-   return axios.put(`https://cloud-sheet.vercel.app/${url}`,data,{withCredentials:true})
+   return axios.put(`https://cloud-sheet.vercel.app/${url}`,data,{withCredentials:true,headers:{'Content-Type':'application/json','Access-Control-Allow-Origin':'*'}})
 }
 
 export const deleteApi=({url}:IdAxios)=>{
-   return axios.delete(`https://cloud-sheet.vercel.app/${url}`,{withCredentials:true})
+   return axios.delete(`https://cloud-sheet.vercel.app/${url}`,{withCredentials:true,headers:{'Content-Type':'application/json','Access-Control-Allow-Origin':'*'}})
 }
