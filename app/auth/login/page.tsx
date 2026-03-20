92cdbdc2e6445237039c2ce6520024e405f90acb
Phdrox <phximenes2103@gmail.com>
2026-03-19 22:58:33 -0300

    add

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -4,12 +4,7 @@ import { getApi } from '@/hooks/requests/api-request'
 import Image from 'next/image'
 
 export default async function Login() {
- try {
-    
-    
-  } catch (e) {
-    console.log("Erro ao buscar API:", e)
-  }
+
 
   return (
     <div className='min-h-screen bg-foreground  gap-3 flex items-center justify-center flex-col'>
