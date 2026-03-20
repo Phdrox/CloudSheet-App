0326a7850698a85394496dc5ddd0b63a5b9b7488
Phdrox <phximenes2103@gmail.com>
2026-03-19 23:22:28 -0300

    proxy

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -2,14 +2,23 @@
 import FormLogin from '@/components/FormLogin'
 import { getApi } from '@/hooks/requests/api-request'
 import Image from 'next/image'
+import { useEffect } from 'react'
 
-export default async function Login() {
- try {
-     const {data}= await getApi({url:'api/category'})
-    console.log(data)
-  } catch (e) {
-    console.log("Erro ao buscar API:", e)
-  }
+export default function Login() {
+
+
+  useEffect( () => {
+    async function fetch(){
+      try {
+        const {data}= await getApi({url:'api/category'})
+        console.log(data)
+      } catch (e) {
+        console.log("Erro ao buscar API:", e)
+      }
+    }
+
+    fetch()
+},[])
 
   return (
     <div className='min-h-screen bg-foreground  gap-3 flex items-center justify-center flex-col'>
