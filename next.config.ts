9731c0a9fb38381f930c6ada0395814e88dee325
Phdrox <phximenes2103@gmail.com>
2026-03-19 23:39:42 -0300

    proxy

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites(){
-    return [{
-      source:"/api/:path*",
-      destination:"https://cloud-sheet.vercel.app/api/:path*"
-    }]
-  }
+async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://cloud-sheet.vercel.app/api/:path*",
+      },
+      {
+        source: "/api/auth/:path*",
+        destination: "https://cloud-sheet.vercel.app/api/auth/:path*",
+      },
+    ]
+  },
 };
 
 export default nextConfig;
