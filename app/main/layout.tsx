81bcaaf6a1ee7d07f0a5c175b3a80317b3a2682f
Phdrox <phximenes2103@gmail.com>
2026-03-19 22:27:13 -0300

    proxy

--- app/main/layout.tsx
+++ app/main/layout.tsx
@@ -1,7 +1,6 @@
 "use client"
 import AppSidebar from "@/components/sidebar"
 import { SidebarProvider,SidebarTrigger } from "@/components/ui/sidebar"
-import { authClient } from "@/lib/auth-client"
 import { useRouter } from "next/navigation"
 
 export default function MainLayout({
@@ -9,18 +8,7 @@ export default function MainLayout({
 }: {
   children: React.ReactNode
 }) {
-  const {data, isPending,error,isRefetching} = authClient.useSession()
   const router = useRouter()
-  
-  console.log(data,isPending,error,isRefetching)
-  if(isPending){
-    return <div>Loading...</div>
-  }
-
-  if(error){
-    return <div>Error: {error.message}</div>
-  }
-  
   
   const menuItems = [
     { name: "Dashboard", icon: "dashboard", url: "/main/dashboard", allowed: ["admin", "all"] },
@@ -34,7 +22,7 @@ export default function MainLayout({
   return (
     <div className="flex bg-accent-foreground">
         <SidebarProvider className="px-3">
-            <AppSidebar array={menuItems} user={data?.user.name!} />
+            
             <SidebarTrigger className="bg-sky-700 text-white cursor-pointer"/>
             <div className="">
               {children}
