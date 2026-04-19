8c585a4e8c3494905a3d1bcda33ca69c43deaded
Phdrox <phximenes2103@gmail.com>
2026-04-18 21:48:28 -0300

    landing

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image'
 
 export default function Login() {
   return (
-    <div className='min-h-screen bg-foreground  gap-1 flex items-center justify-center flex-col'>
-      <Image src={'/LogoCloud.png'} height={250} width={250} alt='logosheetcloud'/>
+    <div className='min-h-screen bg-foreground  flex items-center justify-center flex-col'>
+      <Image src={'/LogoCloud.png'} height={200} width={200} alt='logosheetcloud'/>
       <FormLogin/>
     </div>
   )
